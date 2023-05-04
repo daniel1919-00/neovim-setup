0be95546e4ca22391ac502031020b3776885e15f
Daniel <45707084+daniel1919-00@users.noreply.github.com>
2023-05-04 08:23:00 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -136,7 +136,14 @@ require('lazy').setup({
     {
         "ahmedkhalf/project.nvim",
         config = function()
-            require("project_nvim").setup()
+            require("project_nvim").setup({
+                sync_root_with_cwd = true,
+                respect_buf_cwd = true,
+                update_focused_file = {
+                    enable = true,
+                    update_root = true
+                },
+            })
         end
     },
     {
